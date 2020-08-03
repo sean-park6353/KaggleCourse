1f80b12fe292cf81d3e10a48869f0982dc7f8c50
unknown <package9@naver.com>
2020-08-03 16:41:50 +0900

    캐글 course

---
+++ 1. Python/0.Intro.py
@@ -0,0 +1,8 @@
+'''
+
+Intro
+This course covers the key Python skills you’ll need so you can start using Python for data science. The course is ideal for someone with some previous coding experience who wants to add Python to their repertoire or level up their basic Python skills. (If you're a first-time coder, you may want to check out these "Python for Non-Programmers" learning resources.)
+
+We'll start with a brief overview of Python syntax, variable assignment, and arithmetic operators. If you have previous Python experience, you can skip straight to the hands-on exercise.
+
+'''
---
+++ 1. Python/1.Hell, Python!.py
@@ -0,0 +1,20 @@
+
+#! Hello, Python!
+# ? Python was named for the British comedy troupe Monty Python,
+# ? so we'll make our first Python program an homage to their skit about Spam?
+# ? Just for fun, try reading over the code below and predicting what it's going to do when run.
+# ? (If you have no idea, that's fine!)
+# ? Then click the "output" button to see the results of our program.
+
+
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
