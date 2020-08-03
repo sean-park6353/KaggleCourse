7ec5d2a73018d48ccc89d76c1425f9c8503521b7
unknown <package9@naver.com>
2020-08-03 18:28:18 +0900

    folder

--- 1. Python/2. Functions and Getting help/4. Fuctions Applied to Functions.py
+++ 1. Python/2. Functions and Getting help/4. Fuctions Applied to Functions.py
@@ -23,3 +23,17 @@ print(
     squared_call(mult_by_five, 1),
     sep='\n',  # '\n' is the newline character - it starts a new line
 )
+
+
+def mod_5(x):
+    """Return the remainder of x after dividing by 5"""
+    return x % 5
+
+
+print(
+    'Which number is biggest?',
+    max(100, 51, 14),
+    'Which number is the biggest modulo 5?',
+    max(100, 51, 14, key=mod_5),
+    sep='\n',
+)
