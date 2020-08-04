a3944e1e822d6f315799572cdd8a280a381f57d4
unknown <package9@naver.com>
2020-08-04 23:38:52 +0900

    loop

---
+++ 1. Python/5. Loops/1.py
@@ -0,0 +1,9 @@
+
+
+#! Loops
+# * Loops are a way to repeatedly execute some code.
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars',
+           'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+for planet in planets:
+    print(planet, end=' ')  # print all on same line
---
+++ 1. Python/5. Loops/2.py
@@ -0,0 +1,5 @@
+multiplicands = (2, 2, 2, 3, 3, 5)
+product = 1
+for mult in multiplicands:
+    product = product * mult
+product
