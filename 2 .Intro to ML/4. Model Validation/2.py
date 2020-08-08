49a9fd0d66c6d99ef8d92bd3bf1c6a64b175a351
unknown <package9@naver.com>
2020-08-09 00:29:14 +0900

    differenc validation data with in-sample data

--- 2 .Intro to ML/4. Model Validation/2.py
+++ 2 .Intro to ML/4. Model Validation/2.py
@@ -9,3 +9,32 @@
 # ? and then use those to test the model's accuracy
 # ? on data it hasn't seen before.
 # ? This data is called validation data.
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+import pandas as pd
+path = '../melb_data.csv'
+m_data = pd.read_csv(path)
+m_data = m_data.dropna(axis=0)
+m_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = m_data[m_features]
+y = m_data.Price
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(train_X, train_y)
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
+
+
+# ? Your mean absolute error for the in-sample data was about 1100 dollars.
+# ?  Out-of-sample it is more than 250,000 dollars.
+# ? This is the difference between a model that is
+# ? almost exactly right,
+# ? and one that is unusable for most practical purposes.
+# ? As a point of reference,
+# ? the average home value in the validation data is
+# ? 1.1 million dollars. So the error in new data
+# ? is about a quarter of the average home value.
