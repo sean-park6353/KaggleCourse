54e45cbee0645a424cff839d4ab5d1c47644857b
unknown <package9@naver.com>
2020-08-09 00:24:40 +0900

    The Problem with In-Sample Scores

---
+++ 2 .Intro to ML/4. Model Validation/2.py
@@ -0,0 +1,11 @@
+
+#! The Problem with "In-Sample" Scores
+
+# ? Since models' practical value come from making
+# ? predictions on new data, we measure performance
+# ? on data that wasn't used to build the model.
+# ? The most straightforward way to do this is to exclude
+# ? some data from the model-building process,
+# ? and then use those to test the model's accuracy
+# ? on data it hasn't seen before.
+# ? This data is called validation data.
